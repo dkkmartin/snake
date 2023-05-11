6b71e0818a7765f6aa940cbaa8e150f0c629bfd6
Martin Bruun <martinbruun1995@gmail.com>
2023-05-11 12:55:46 +0200

    Add: logic

--- main.js
+++ main.js
@@ -5,18 +5,24 @@ const mapBig = document.querySelector('.game__start__btn--big')
 const bigSizeMap = 2000
 const mediumSizeMap = 1500
 const smallSizeMap = 1000
+let selectedMapSize
+let boardArray
+let currentSnakePos
 
 mapSmall.addEventListener('click', () => {
-  gameRound(smallSizeMap)
+  selectedMapSize = smallSizeMap
+  startGame(selectedMapSize)
 })
 mapMedium.addEventListener('click', () => {
-  gameRound(mediumSizeMap)
+  selectedMapSize = mediumSizeMap
+  startGame(selectedMapSize)
 })
 mapBig.addEventListener('click', () => {
-  gameRound(bigSizeMap)
+  selectedMapSize = bigSizeMap
+  startGame(selectedMapSize)
 })
 
-function appendCubes(mapSize) {
+function buildMap(mapSize) {
   if(board.hasChildNodes()) {
     board.innerHTML = ''
   }
@@ -37,24 +43,110 @@ function appendCubes(mapSize) {
   }
 }
 
-function gameRound(mapSize) {
-  const randomPosForApple = apple(mapSize)
-  appendCubes(mapSize)
-  gameBoardArray(randomPosForApple)
+function startGame(mapSize) {
+  buildMap(mapSize)
   board.style.display = 'grid'
-  gameBoardArray[randomPosForApple].style.backgroundColor = 'red'
+  boardArray = Array.from(document.querySelectorAll('.board__cube'))
+  apple(mapSize)
+  snake(mapSize)
 }
 
-function snake() {
+(function handleInputForSnake () {
+  document.addEventListener('keydown', (event) => {
+    if(event.code === 'ArrowDown') {
+      sendSnakeDown(currentSnakePos, selectedMapSize)
+    }
+    if(event.code === 'ArrowUp') {
+      sendSnakeUp(currentSnakePos, selectedMapSize)
+    }
+    if(event.code === 'ArrowLeft') {
+      sendSnakeLeft(currentSnakePos, selectedMapSize)
+    }
+    if(event.code === 'ArrowRight') {
+      sendSnakeRight(currentSnakePos, selectedMapSize)
+    }
+  })
+})()
 
+function snake(mapSize) {
+  if(mapSize === 1000) {
+    boardArray[500].classList.add('snake')
+    currentSnakePos = 500
+  }
+  if(mapSize === 1500) {
+    boardArray[725].classList.add('snake')
+    currentSnakePos = 725
+  }
+  if(mapSize === 2000) {
+    boardArray[975].classList.add('snake')
+    currentSnakePos = 975
+  }
+}
+// if pos is undefined snake hit a wall and should game over
+function sendSnakeDown(pos, mapSize) {
+  if(mapSize === 1000) {
+    pos = pos + 40
+    boardArray[pos].classList.add('snake')
+    boardArray[pos - 40].classList.remove('snake')
+    currentSnakePos = pos
+  }
+  if(mapSize === 1500) {
+    pos = pos + 50
+    boardArray[pos].classList.add('snake')
+    boardArray[pos - 50].classList.remove('snake')
+    currentSnakePos = pos
+  }
+  if(mapSize === 2000) {
+    pos = pos + 50
+    boardArray[pos].classList.add('snake')
+    boardArray[pos - 50].classList.remove('snake')
+    currentSnakePos = pos
+  }
 }
 
-function gameBoardArray(position) {
-  const boardArray = Array.from(document.querySelectorAll('.board__cube'))
-  
+function sendSnakeUp(pos, mapSize) {
+  if(mapSize === 1000) {
+    pos = pos - 40
+    boardArray[pos].classList.add('snake')
+    boardArray[pos + 40].classList.remove('snake')
+    currentSnakePos = pos
+  }
+  if(mapSize === 1500) {
+    pos = pos - 50
+    boardArray[pos].classList.add('snake')
+    boardArray[pos + 50].classList.remove('snake')
+    currentSnakePos = pos
+  }
+  if(mapSize === 2000) {
+    pos = pos - 50
+    boardArray[pos].classList.add('snake')
+    boardArray[pos + 50].classList.remove('snake')
+    currentSnakePos = pos
+  }
+}
+
+function sendSnakeLeft(pos) {
+  pos = pos - 1
+  boardArray[pos].classList.add('snake')
+  boardArray[pos + 1].classList.remove('snake')
+  currentSnakePos = pos
+}
+
+function sendSnakeRight(pos) {
+  if(
+    pos === 39 || pos === 79 || pos === 119 || pos === 159 || pos === 199 ||
+    pos === 239 || pos === 279 || pos === 319 || pos === 359 || pos === 399 || pos === 439 || pos === 479 || pos === 519 || pos === 559 || pos === 599 || pos === 639 || pos === 679 || pos === 719 || pos === 759 || pos === 799 || pos === 839 || pos === 879 || pos === 919 || pos === 959 || pos === 999 
+    ) {
+      // Call game over function here
+    console.log('game over')
+  }
+  pos = pos + 1
+  boardArray[pos].classList.add('snake')
+  boardArray[pos - 1].classList.remove('snake')
+  currentSnakePos = pos
 }
 
 function apple(mapSize) {
   const randomPos = Math.floor(Math.random() * mapSize) + 1
-  return randomPos
+  boardArray[randomPos].classList.add('apple')
 }
