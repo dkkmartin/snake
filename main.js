6a86112e568ebeade20d844d557a367eb5ba70cf
Martin <martinbruun1995@gmail.com>
2023-05-11 23:02:49 +0200

    Add: Snake logic

--- main.js
+++ main.js
@@ -2,12 +2,18 @@ const board = document.querySelector('.board')
 const mapSmall = document.querySelector('.game__start__btn--small')
 const mapMedium = document.querySelector('.game__start__btn--medium')
 const mapBig = document.querySelector('.game__start__btn--big')
+const highScore = document.querySelector('.highscore')
+const gameInfo = document.querySelector('.game__info')
+const mapSelectorButtons = document.querySelector('.size__selector__con')
 const bigSizeMap = 2000
 const mediumSizeMap = 1500
 const smallSizeMap = 1000
 let selectedMapSize
 let boardArray
 let currentSnakePos
+let isSnakeMoving = false
+let snakeTimeInterval
+let lastArrowKeyPressed
 
 mapSmall.addEventListener('click', () => {
   selectedMapSize = smallSizeMap
@@ -48,27 +54,34 @@ function startGame(mapSize) {
   board.style.display = 'grid'
   boardArray = Array.from(document.querySelectorAll('.board__cube'))
   apple(mapSize)
-  snake(mapSize)
+  isSnakeMoving = true
+  placeSnake(mapSize)
+  highScore.style.display = 'block'
+  mapSelectorButtons.style.display = 'none'
 }
 
 (function handleInputForSnake () {
   document.addEventListener('keydown', (event) => {
     if(event.code === 'ArrowDown') {
       sendSnakeDown(currentSnakePos, selectedMapSize)
+      snakeMover()
     }
     if(event.code === 'ArrowUp') {
       sendSnakeUp(currentSnakePos, selectedMapSize)
+      snakeMover()
     }
     if(event.code === 'ArrowLeft') {
       sendSnakeLeft(currentSnakePos, selectedMapSize)
+      snakeMover()
     }
     if(event.code === 'ArrowRight') {
       sendSnakeRight(currentSnakePos, selectedMapSize)
+      snakeMover()
     }
   })
 })()
 
-function snake(mapSize) {
+function placeSnake(mapSize) {
   if(mapSize === 1000) {
     boardArray[500].classList.add('snake')
     currentSnakePos = 500
@@ -84,13 +97,14 @@ function snake(mapSize) {
 }
 // if pos is undefined snake hit a wall and should game over
 function sendSnakeDown(pos, mapSize) {
+  lastArrowKeyPressed = sendSnakeDown
   if(mapSize === 1000) {
     pos = pos + 40
     try {
       boardArray[pos].classList.add('snake')
     } catch (error) {
       // Call game over function here
-      console.log('game over')
+      gameOver()
     }
     boardArray[pos - 40].classList.remove('snake')
     currentSnakePos = pos
@@ -101,7 +115,7 @@ function sendSnakeDown(pos, mapSize) {
       boardArray[pos].classList.add('snake')
     } catch (error) {
       // Call game over function here
-      console.log('game over')
+      gameOver()
     }
     boardArray[pos - 50].classList.remove('snake')
     currentSnakePos = pos
@@ -112,7 +126,7 @@ function sendSnakeDown(pos, mapSize) {
       boardArray[pos].classList.add('snake')
     } catch (error) {
       // Call game over function here
-      console.log('game over')
+      gameOver()
     }
     boardArray[pos - 50].classList.remove('snake')
     currentSnakePos = pos
@@ -120,13 +134,14 @@ function sendSnakeDown(pos, mapSize) {
 }
 
 function sendSnakeUp(pos, mapSize) {
+  lastArrowKeyPressed = sendSnakeUp
   if(mapSize === 1000) {
     pos = pos - 40
     try {
       boardArray[pos].classList.add('snake')
     } catch (error) {
       // Call game over function here
-      console.log('game over')
+      gameOver()
     }
     boardArray[pos + 40].classList.remove('snake')
     currentSnakePos = pos
@@ -137,7 +152,7 @@ function sendSnakeUp(pos, mapSize) {
       boardArray[pos].classList.add('snake')
     } catch (error) {
       // Call game over function here
-      console.log('game over')
+      gameOver()
     }
     boardArray[pos + 50].classList.remove('snake')
     currentSnakePos = pos
@@ -148,7 +163,7 @@ function sendSnakeUp(pos, mapSize) {
       boardArray[pos].classList.add('snake')
     } catch (error) {
       // Call game over function here
-      console.log('game over')
+      gameOver()
     }
     boardArray[pos + 50].classList.remove('snake')
     currentSnakePos = pos
@@ -156,45 +171,73 @@ function sendSnakeUp(pos, mapSize) {
 }
 
 function sendSnakeLeft(pos) {
-  if(
-    pos === 40 || pos === 80 || pos === 120 || pos === 160 || pos === 200 ||
-    pos === 240 || pos === 280 || pos === 320 || pos === 360 || pos === 400 || pos === 440 || pos === 480 || pos === 520 || pos === 560 || pos === 600 || pos === 640 || pos === 680 || pos === 720 || pos === 760 || pos === 800 || pos === 840 || pos === 880 || pos === 920 || pos === 960 || pos === 1000
-    ) {
-      // Call game over function here
-    console.log('game over')
+  lastArrowKeyPressed = sendSnakeLeft
+  if(selectedMapSize === 1000) {
+    if(pos % 40 === 0) {
+        // Call game over function here
+      gameOver()
+    }
+  }
+  if(selectedMapSize === 1500 || selectedMapSize === 2000) {
+    if(pos % 50 === 0) {
+        // Call game over function here
+      gameOver()
+    }
   }
   pos = pos - 1
   try {
     boardArray[pos].classList.add('snake')
   } catch (error) {
     // Call game over function here
-    console.log('game over')
+    gameOver()
   }
   boardArray[pos + 1].classList.remove('snake')
   currentSnakePos = pos
 }
 
 function sendSnakeRight(pos) {
-  if(
-    pos === 39 || pos === 79 || pos === 119 || pos === 159 || pos === 199 ||
-    pos === 239 || pos === 279 || pos === 319 || pos === 359 || pos === 399 || pos === 439 || pos === 479 || pos === 519 || pos === 559 || pos === 599 || pos === 639 || pos === 679 || pos === 719 || pos === 759 || pos === 799 || pos === 839 || pos === 879 || pos === 919 || pos === 959 || pos === 999
-    ) {
-      // Call game over function here
-    console.log('game over')
+  lastArrowKeyPressed = sendSnakeRight
+  if(selectedMapSize === 1000) {
+    if(pos % 40 === 39) {
+        // Call game over function here
+      gameOver()
+    }
+  }
+  if(selectedMapSize === 1500 || selectedMapSize === 2000) {
+    if(pos % 50 === 49) {
+        // Call game over function here
+      gameOver()
+    }
   }
   pos = pos + 1
   try {
     boardArray[pos].classList.add('snake')
   } catch (error) {
     // Call game over function here
-    console.log('game over')
+    gameOver()
   }
   boardArray[pos - 1].classList.remove('snake')
   currentSnakePos = pos
 }
 
 function gameOver() {
+  stopSnake()
+  gameInfo.style.display = 'block'
+  gameInfo.textContent = 'You died'
+  board.style.display = 'none'
+}
 
+function stopSnake () {
+  clearInterval(snakeTimeInterval)
+}
+
+function snakeMover() {
+  if(isSnakeMoving) {
+    clearInterval(snakeTimeInterval)
+  }
+  snakeTimeInterval = setInterval(() => {
+    lastArrowKeyPressed(currentSnakePos, selectedMapSize)
+  }, 200)
 }
 
 function apple(mapSize) {
